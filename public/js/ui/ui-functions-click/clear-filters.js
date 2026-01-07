3049c75067a5ef7517b3ce34c5110bdb6e14a825
aewshopping <aewshopping@gmail.com>
2026-01-07 17:51:22 +0000

    clears search box on clear click

--- public/js/ui/ui-functions-click/clear-filters.js
+++ public/js/ui/ui-functions-click/clear-filters.js
@@ -3,14 +3,13 @@ import { renderData } from '../ui-functions-render/render-all-files.js';
 
 
 export function handleClearFilters() {
+    const searchBox = document.getElementById("searchbox");
 
     appState.filterTags.clear();
-
-    // 2. Reset the 'show' status of all file objects (The specific request)
-    appState.myFiles.forEach(file => {
+    appState.myFiles.forEach((file) => {
         file.show = true;
-    });    
+    });
+    renderData(false);
 
-    renderData(false); // full render = false for table view where we only want to re render rows not header
-    
+    searchBox.value = "";
 }
