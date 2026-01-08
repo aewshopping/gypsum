529ee2f6473ed55f99a8dae96ecd97bfcbc5f044
aewshopping <aewshopping@gmail.com>
2026-01-08 08:11:37 +0000

    Update filter-files.js
    change to target rather than event to capture data

--- public/js/ui/ui-functions-click/filter-files.js
+++ public/js/ui/ui-functions-click/filter-files.js
@@ -58,11 +58,13 @@ export function updateMyFilesShowState() {
 /**
  * Handles the click event on a tag element, updating the appState.filterTags Set
  * and then updating the 'show' key in the appState.myFiles array.
- * @param {Event} evt The click event object.
+ * @param {Event} evt The click event object
+ * @param {Target} target The target object
  */
-export function handleTagClick(evt) {
+export function handleTagClick(evt, target) {
 
-    const tagName = evt.target.classList[1];
+// this isn't checking for tag in classList[0] as it should be...
+    const tagName = target.classList[1];
     if (!tagName) {
         console.error("Clicked element does not have a tag class at index 1.");
         return;
