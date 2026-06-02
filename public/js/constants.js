07345b799d5a953659064147aa3262a13ef1394d
aewshopping <aewshopping@gmail.com>
2026-06-02 09:29:43 +0100

    Add new pastel color constants for red

--- public/js/constants.js
+++ public/js/constants.js
@@ -26,6 +26,7 @@ export function setPaginationSize(n) { PAGINATION_SIZE = n; }
 export const PROGRESS_STEP_SIZE = 1; // update loading progress every N percentage points
 
 export const COLOR_NAMES = [
+  "#ffbdbd", // Pastel Red
   "#f0cbc8", // Pastel Rose
   "#f0d8c8", // Pastel Terracotta
   "#f0e6c8", // Pastel Ochre
